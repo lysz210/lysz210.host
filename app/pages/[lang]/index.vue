b76d42effd1dec0e409d48d47d828c64098d9174
Lingyong Sun <lingyong.sun@gmail.com>
2026-01-04 18:40:31 +0100

    first commit

---
+++ app/pages/[lang]/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-layout full-height>
+    <v-card class="h-100 w-100">
+      <iframe style="width: 100%; height: 100%" ref="externalWindow" :src="src" frameborder="0"></iframe>
+      <v-btn :href="pdfSrc" target="_blank" icon="fas fa-print" dark color="green" position="absolute" elevation="12"
+        class="print-btn"></v-btn>
+    </v-card>
+  </v-layout>
+</template>
+<style>
+.print-btn {
+  bottom: 16px;
+  right: 32px;
+}
+</style>
+  
+<script>
+export default {
+  data() {
+    const {
+      public: {
+        cvBasePath,
+        cvPdf
+      }
+    } = useRuntimeConfig()
+    return {
+      fab: false,
+      cv: {
+        basePath: cvBasePath,
+        pdf: cvPdf
+      }
+    };
+  },
+  computed: {
+    lang() {
+      console.log(this)
+      return this.$route?.params?.lang || 'en'
+    },
+    src() {
+      return `${this.cv.basePath}/${this.lang}`
+    },
+    pdfSrc() {
+      return `https://lysz210.github.io/profile/i18n/${this.lang}/cv.pdf`
+    }
+  }
+};
+</script>
+  
---
+++ nuxt.config.ts
@@ -0,0 +1,43 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/hints',
+    '@pinia/nuxt',
+  ],
+  nitro: {
+    prerender: {
+      routes: ['/it', '/en']
+    }
+  },
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    plugins: [vuetify({ autoImport: true })],
+    vue: {
+      template: {
+        transformAssetUrls
+      }
+    }
+  },
+  ssr: false,
+  app: {
+      head: {
+          title: 'Lysz210'
+      }
+  },
+
+  runtimeConfig: {
+      public: {
+          cvBasePath: 'https://cv.lysz210.me',
+          cvPdf: '/CV_lingyong_sun.pdf',
+          meSocialAccounts: '/me/social-accounts'
+      }
+  },
+
+})
